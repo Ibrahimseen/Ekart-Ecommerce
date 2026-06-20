68e434c29e506925138b697d088def6d2042540d
Ibrahimseen <ibrahimrehmanibrahim@gmail.com>
2026-06-20 17:50:08 +0500

    fix: updated CORS origin for production frontend

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,9 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    // origin: "http://localhost:5173",
+     origin: "https://ekart-frontend-ug4u.onrender.com/",
+    
     credentials: true,
   }),
 );
